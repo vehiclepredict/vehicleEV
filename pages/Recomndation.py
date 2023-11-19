6de0fb8ffdb1bb508e003c105fd0208ebdd2da53
vehiclepredict <150584433+vehiclepredict@users.noreply.github.com>
2023-11-19 09:31:30 +0530

    Update Recomndation.py

--- pages/Recomndation.py
+++ pages/Recomndation.py
@@ -8,7 +8,7 @@ st.header('Survey Results 2021')
 st.subheader('Was the tutorial helpful?')
 
 ### --- LOAD DATAFRAME
-excel_file = 'saved_model/Survey_Results.xlsx'
+excel_file = 'saved_model/EV.xlsx'
 sheet_name = 'DATA'
 
 df = pd.read_excel(excel_file,
@@ -23,32 +23,32 @@ df_participants = pd.read_excel(excel_file,
 df_participants.dropna(inplace=True)
 
 # --- STREAMLIT SELECTION
-department = df['Department'].unique().tolist()
-ages = df['Age'].unique().tolist()
+department = df['BreakDown'].unique().tolist()
+year = df['Model Year'].unique().tolist()
 
-age_selection = st.slider('Age:',
-                        min_value= min(ages),
-                        max_value= max(ages),
-                        value=(min(ages),max(ages)))
+age_selection = st.slider('Year:',
+                        min_value= min(year),
+                        max_value= max(year),
+                        value=(min(year),max(year)))
 
-department_selection = st.multiselect('Department:',
+department_selection = st.multiselect('BreakDown:',
                                     department,
                                     default=department)
 
 # --- FILTER DATAFRAME BASED ON SELECTION
-mask = (df['Age'].between(*age_selection)) & (df['Department'].isin(department_selection))
+mask = (df['Model Year'].between(*age_selection)) & (df['BreakDown'].isin(department_selection))
 number_of_result = df[mask].shape[0]
 st.markdown(f'*Available Results: {number_of_result}*')
 
 # --- GROUP DATAFRAME AFTER SELECTION
-df_grouped = df[mask].groupby(by=['Rating']).count()[['Age']]
-df_grouped = df_grouped.rename(columns={'Age': 'Votes'})
+df_grouped = df[mask].groupby(by=['Make']).count()[['Year']]
+df_grouped = df_grouped.rename(columns={'Model Year': 'Model Year'})
 df_grouped = df_grouped.reset_index()
 
 # --- PLOT BAR CHART
 bar_chart = px.bar(df_grouped,
-                   x='Rating',
-                   y='Votes',
+                   x='Reccomand',
+                   y='Model Year',
                    text='Votes',
                    color_discrete_sequence = ['#F63366']*len(df_grouped),
                    template= 'plotly_white')
